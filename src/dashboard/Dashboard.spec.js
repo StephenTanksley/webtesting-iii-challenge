5eecb2a7fa3dc270e6e01e7464a9ec30185fb75d
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-30 18:20:15 -0500

    Worked on getting some boilerplate testing imports into the controls.spec.js and display.spec.js files.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,9 +1,7 @@
 // Test away!
 
 import React from 'react'
-import renderer from 'react-test-renderer'
 import * as rtl from '@testing-library/react'
-
 import "@testing-library/jest-dom/extend-expect"
 
 import Dashboard from './Dashboard'
@@ -13,12 +11,16 @@ afterEach(rtl.cleanup);
 
 //testing to make sure the dashboard shows up.
 describe('<Dashboard />', () => {
+    //we describe the test we're implementing.
     it('should match snapshot', () => {
+
+        //we target the component we want to test here.
         const wrapper = rtl.render(<Dashboard />)
 
+        //we're using the debug function to visualize the element tree in the terminal.
         wrapper.debug()
 
-        //we're expecting the dashboard to match the snapshot we created.
+        //we expecting the dashboard to match the saved snapshot.
         expect(wrapper.asFragment()).toMatchSnapshot();
 
 
