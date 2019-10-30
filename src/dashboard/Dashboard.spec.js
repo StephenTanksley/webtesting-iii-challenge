ef655c30ae707aee27f2362ac0cf2d75ec0e6447
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-30 18:17:09 -0500

    Implemented a snapshot test for the dashboard component.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,26 @@
-// Test away
+// Test away!
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+import * as rtl from '@testing-library/react'
+
+import "@testing-library/jest-dom/extend-expect"
+
+import Dashboard from './Dashboard'
+
+
+afterEach(rtl.cleanup);
+
+//testing to make sure the dashboard shows up.
+describe('<Dashboard />', () => {
+    it('should match snapshot', () => {
+        const wrapper = rtl.render(<Dashboard />)
+
+        wrapper.debug()
+
+        //we're expecting the dashboard to match the snapshot we created.
+        expect(wrapper.asFragment()).toMatchSnapshot();
+
+
+    });
+});
